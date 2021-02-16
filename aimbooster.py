1698ce6f2eab6b1a505cc6c7a6dd60cc5b3fd958
setup425 <serdeltomas@gmail.com>
2021-02-16 16:26:50 +0100

    removed unnecessary comments

--- aimbooster.py
+++ aimbooster.py
@@ -14,21 +14,17 @@ def click(x,y):
     time.sleep(0.01)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
-#Color of center: (255, 219, 197)
 lastclickx=0
 lastclicky=0
 screenx = 670
 screeny = 340
 count = 0
+
 while keyboard.is_pressed('q') == False:
-    
     pic = pyautogui.screenshot(region=(screenx,screeny,600,420))
-
     width, height = pic.size
-
     for x in range(0,width,5):
         for y in range(0,height,5):
-            ##print(".",end="")
             r,g,b = pic.getpixel((x,y))
             if b == 197 and x != lastclickx and y != lastclicky:
                 click(x+screenx,y+screeny)
@@ -36,5 +32,4 @@ while keyboard.is_pressed('q') == False:
                 print("n°" + str(count) + "\t| " + str(x) + " " + str(y))
                 lastclickx=x
                 lastclicky=y
-                ##time.sleep(0.05)
                 break
